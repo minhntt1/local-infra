7f9020227827363a6d68a5a03ef62be55605db03
minhntt1 <89958007+minhntt1@users.noreply.github.com>
2026-07-23 23:07:21 +0700

    chore: revert

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,9 +9,9 @@ terraform {
 }
 
 provider "proxmox" {
-  # endpoint  = var.pm_api_url
-  # api_token = "${var.pm_api_token_id}=${var.pm_api_token_secret}"
-  # insecure  = true
+  endpoint  = var.pm_api_url
+  api_token = "${var.pm_api_token_id}=${var.pm_api_token_secret}"
+  insecure  = true
 
   ssh {
     agent       = false
